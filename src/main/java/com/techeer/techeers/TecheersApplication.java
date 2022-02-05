597237c5762009dc2f49f6e2991c46a7c46a2ae5
Soobin Jung <judyb0626@gmail.com>
2022-02-04 21:33:36 -0800

    build: initialize Spring Boot project

---
+++ src/main/java/com/techeer/techeers/TecheersApplication.java
@@ -0,0 +1,13 @@
+package com.techeer.techeers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecheersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecheersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techeer/techeers/TecheersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techeer.techeers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecheersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
